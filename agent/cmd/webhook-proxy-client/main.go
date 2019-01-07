cbacfd7efef754989f8f1c914db98458e2a59346
Chris Gascoigne <cgascoig@cisco.com>
2019-01-08 08:42:46 +1100

    refactor agent to automatically reconnect on failure

--- agent/cmd/webhook-proxy-client/main.go
+++ agent/cmd/webhook-proxy-client/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -14,6 +17,14 @@ var (
 	verbose        bool
 	url            string
 	subscriptionID string
+	targetUrl      string
+)
+
+const (
+	pongWait   = 10 * time.Second
+	pingPeriod = (pongWait * 9) / 10
+
+	reconnectDelay = 5 * time.Second
 )
 
 type SubscribeMessage struct {
@@ -25,30 +36,40 @@ type ReceivedMessage struct {
 	Action         string `json:"action"`
 	SubscriptionID string `json:"subscriptionId"`
 	Body           string `json:"body"`
+	ContentType    string `json:"contentType"`
 }
 
-func main() {
-	flag.Parse()
+func receivedMessage(msg ReceivedMessage, log *logrus.Logger) {
+	log.Debug("ReadMessage: ", msg)
 
-	log := logrus.New()
-	if verbose {
-		log.Level = logrus.DebugLevel
-		log.Debug("Logging level set to debug")
+	_, err := http.Post(targetUrl, msg.ContentType, strings.NewReader(msg.Body))
+	if err != nil {
+		log.Error("Sending POST to targetUrl: ", err)
 	}
+}
 
-	if subscriptionID == "" {
-		log.Fatal("subscriptionId must be set")
-	}
-
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
+func startConnection(log *logrus.Logger, interrupt chan os.Signal) bool {
+	shuttingDown := false
 
 	con, _, err := websocket.DefaultDialer.Dial(url, nil)
 	if err != nil {
-		log.Fatal("Connection failed: ", err)
+		log.Error("Connection failed: ", err)
+		return shuttingDown
 	}
 
-	defer con.Close()
+	ticker := time.NewTicker(pingPeriod)
+
+	defer func() {
+		con.Close()
+		ticker.Stop()
+	}()
+
+	con.SetReadDeadline(time.Now().Add(pongWait))
+	con.SetPongHandler(func(string) error {
+		log.Debug("Received keepalive response (pong)")
+		con.SetReadDeadline(time.Now().Add(pongWait))
+		return nil
+	})
 
 	done := make(chan struct{})
 	go func() {
@@ -61,7 +82,8 @@ func main() {
 				return
 			}
 
-			log.Info("ReadMessage: ", message)
+			receivedMessage(message, log)
+
 		}
 	}()
 
@@ -79,21 +101,63 @@ func main() {
 
 	for {
 		select {
+		case <-ticker.C:
+			log.Debug("Sending keepalive request (ping)")
+			if err := con.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
+				log.Error("Error sending keepalive: ", err)
+				return shuttingDown
+			}
 		case <-done:
-			return
+			return shuttingDown
 		case <-interrupt:
 			log.Info("Shutting down")
+			shuttingDown = true
+
 			err = con.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
 			if err != nil {
 				log.Error("WriteMessage (closing) failed: ", err)
-				return
+				return shuttingDown
 			}
 			select {
 			case <-done:
 			case <-time.After(time.Second):
-				return
+				return shuttingDown
 			}
 		}
+	}
+
+}
+
+func main() {
+	flag.Parse()
+
+	log := logrus.New()
+	if verbose {
+		log.Level = logrus.DebugLevel
+		log.Debug("Logging level set to debug")
+	}
+
+	if subscriptionID == "" {
+		log.Fatal("subscriptionId must be set")
+	}
+
+	if targetUrl == "" {
+		log.Fatal("targerUrl must be set")
+	}
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	for {
+		log.Info("Starting connection")
+		shuttingDown := startConnection(log, interrupt)
+
+		if shuttingDown {
+			return
+		}
+
+		log.Info(fmt.Sprintf("Waiting %v seconds to reconnect ...", reconnectDelay))
+		time.Sleep(reconnectDelay)
 	}
 }
 
@@ -101,4 +165,5 @@ func init() {
 	flag.BoolVar(&verbose, "verbose", false, "Verbose logging")
 	flag.StringVar(&url, "url", "wss://localhost", "WebSocket URL of webhook-proxy cloud component")
 	flag.StringVar(&subscriptionID, "subscriptionId", "", "Unique subscriptionId")
+	flag.StringVar(&targetUrl, "targetUrl", "", "Internal URL where proxied webhook events should be sent")
 }
